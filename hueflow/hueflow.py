3c9d6fdd01a4990656a45b051174faa851b032e8
Raphael Coelho <raphaelrrcoelho@gmail.com>
2017-12-06 17:51:37 -0200

    adiciona no nas saidas dos nos de entrada

--- hueflow/hueflow.py
+++ hueflow/hueflow.py
@@ -2,4 +2,9 @@ import numpy as np
 
 class No(object):
     def __init__(self, nos_entrada = []):
-       self.nos_entrada = nos_entrada 
+       self.nos_entrada = nos_entrada
+
+       self.nos_saida = []
+
+       for no in self.nos_entrada:
+           no.nos_saida.append(self)
--- testes/teste_hueflow.py
+++ testes/teste_hueflow.py
@@ -2,13 +2,18 @@ import unittest
 from hueflow.hueflow import No
 
 class TesteNo(unittest.TestCase):
-
-    def teste_criar_no_com_lista_de_nos_de_entrada(self):
+    
+    def setUp(self):
        no_1 = No()
        no_2 = No()
-       no_3 = No(nos_entrada = [no_1, no_2])
-
-       self.assertEqual(no_3.nos_entrada, [no_1, no_2])
+       self.nos_entrada = [no_1, no_2]
+        
+    def teste_criar_no_com_lista_de_nos_de_entrada(self):
+       no_teste = No(nos_entrada = self.nos_entrada)
+       self.assertEqual(no_teste.nos_entrada, self.nos_entrada)
 
     def teste_adicionar_a_si_como_saida_dos_seus_nos_de_entrada(self):
-        pass
+        no_teste = No(nos_entrada = self.nos_entrada)
+        
+        self.assertTrue(no_teste in self.nos_entrada[0].nos_saida)
+        self.assertTrue(no_teste in self.nos_entrada[1].nos_saida)
